af7b8b81949a4292a5afe8fc623f7109e48a1677
Jaffar <jaffar.shaik706@gmail.com>
2024-06-08 12:29:47 +0200

    add login

---
+++ src/test/java/openOrangeCRM/login.java
@@ -0,0 +1,18 @@
+package openOrangeCRM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class login {
+	WebDriver driver;
+	@Test
+	public void loginTest()
+	{
+		driver =new ChromeDriver();
+		driver.get("https://www.orangecrm.com/");
+		driver.manage().window().maximize();
+	
+	}
+
+}
