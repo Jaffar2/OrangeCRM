a308fc1179b8b20869d66c71fa7cb213ef3b30ff
Jaffar <jaffar.shaik706@gmail.com>
2024-06-08 19:30:55 +0200

    click feature

--- src/test/java/openOrangeCRM/login.java
+++ src/test/java/openOrangeCRM/login.java
@@ -1,12 +1,15 @@
 package openOrangeCRM;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class login {
 	WebDriver driver;
-	@Test
+	@BeforeTest
 	public void loginTest()
 	{
 		driver =new ChromeDriver();
@@ -14,6 +17,17 @@ public class login {
 		driver.manage().window().maximize();
 		System.out.print("Browser open");
 	
+	}
+	@Test
+	public void clickFeature()
+	{
+	driver.findElement(By.xpath("//a[normalize-space()='FEATURES']")).click();	
+	System.out.print("Feature click");
+	}
+	@AfterTest
+	public void close()
+	{
+		driver.close();
 	}
 
 }
